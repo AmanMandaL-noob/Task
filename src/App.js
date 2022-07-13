77bc8a734c2adef455fbeb7c3ca87cdf71a0c395
AmanMandaL-noob <67774794+AmanMandaL-noob@users.noreply.github.com>
2022-07-13 21:33:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Product from './components/Product';
+
+function App() {
+  return <div className="App">
+    <Navbar />
+    <Product />
+    <Footer />
+  </div>
+}
+
+export default App;
